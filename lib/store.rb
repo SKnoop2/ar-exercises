e824693ac81e78eb962b18eff5115d893dd53325
vagrant <sknoop@hotmail.ca>
2021-03-30 18:21:14 +0000

    validate store data

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,6 @@
 class Store < ActiveRecord::Base
-
+  has_many :employees
+  validates :name, length: { minimum: 3}, presence: true
+  validates :annual_revenue, numericality: { only_integer: true, minimum: 0 }
+  validates_associated :employees
 end
